79c1a05fdd703aacdc77bd1029778c3047a85f1a
ivangor246 <ivangor246@gmail.com>
2025-07-18 14:53:12 +0300

    Add config and lifespan to fastapi app

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from app.core.config import config
+from app.core.lifespan import lifespan
 
 
 def create_app() -> FastAPI:
-    app = FastAPI()
-
-    @app.get('/')
-    def hello():
-        return 'Hello'
+    app = FastAPI(
+        title=config.TITLE,
+        docs_url=config.DOCS_URL,
+        openapi_url=config.OPENAPI_URL,
+        default_response_class=JSONResponse,
+        lifespan=lifespan,
+    )
 
     return app
