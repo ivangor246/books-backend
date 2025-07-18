ef74cdff137361d79d2e5c24ea3dba25dcd03055
ivangor246 <ivangor246@gmail.com>
2025-07-18 15:06:37 +0300

    Setup lifespan

--- src/app/core/lifespan.py
+++ src/app/core/lifespan.py
@@ -2,7 +2,10 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from app.core import database
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    await database.init_models()
     yield
